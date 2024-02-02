2fbe2e934710e5b5668b1ee2da59e8c21d49c6a1
VCOliver <victorcruz.01@gmail.com>
2024-02-02 18:44:12 -0300

    Writes statistics to formated txt file

--- join_files.py
+++ join_files.py
@@ -1,8 +1,12 @@
 import os
 import pprint
 from chardet import detect
+import pandas as pd
+from tabulate import tabulate
+import sys
 
 MEASUREMENTS_PATH = 'measurements/test_240202'
+MEASUREMENTS_STATS_PATH = 'files_statistics'
 
 printer = pprint.PrettyPrinter()
 pp = printer.pprint
@@ -18,7 +22,27 @@ def get_file_encoding(file_name: str) -> str:
     print(f"The file's encoding is [{encoding}] with {result['confidence']*100}% confidence.") 
     return encoding
 
-if __name__ == '__main__':
+def main(table_styling='rounded_outline'):
     encoding = get_file_encoding('test_1.txt')
+    header = ['time', 'x', 'y', 'z']
+    df = pd.read_csv(f'{MEASUREMENTS_PATH}/test_1.txt', sep='\t', decimal=',', encoding=encoding, header=None, names=header)
     
+    if not os.path.exists(MEASUREMENTS_STATS_PATH):
+        os.makedirs(MEASUREMENTS_STATS_PATH)
+    file_stats = df.describe()
+    file_path = f'{MEASUREMENTS_STATS_PATH}/test_1_stats.txt'
+    
+    table = tabulate(file_stats, headers='keys', tablefmt=table_styling, floatfmt=".7f")
+    
+    with open(file_path, 'w', encoding=encoding) as file:
+        file.write(table)
+
+
+if __name__ == '__main__':
+    table_styling = None
+    if len(sys.argv) > 1:
+        table_styling = sys.argv[1]
+        main(table_styling)
+    else:  
+        main()
     
