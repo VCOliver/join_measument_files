a32534fb08ce3bb4f5c41bfb09d4acbb2cfdd28e
VCOliver <victorcruz.01@gmail.com>
2024-02-02 17:49:26 -0300

    getting files encodings

--- join_files.py
+++ join_files.py
@@ -1,17 +1,24 @@
 import os
 import pprint
+from chardet import detect
 
 MEASUREMENTS_PATH = 'measurements/test_240202'
 
 printer = pprint.PrettyPrinter()
 pp = printer.pprint
 
+def get_file_encoding(file_name: str) -> str:
+    file_path = f'{MEASUREMENTS_PATH}/{file_name}'
+    
+    with open(file_path, 'rb') as file:
+        result = detect(file.read())
+       
+    encoding = result['encoding'] if result['encoding'] != 'ascii' else 'utf-8' #returns utf-8 if encoding in ASCII
+       
+    print(f"The file's encoding is [{encoding}] with {result['confidence']*100}% confidence.") 
+    return encoding
+
 if __name__ == '__main__':
-    files = os.listdir(MEASUREMENTS_PATH)
-    for file in files:
-        new_name = file.replace('t.', 't_')
-        old_path = os.path.join(MEASUREMENTS_PATH, file)
-        new_path = os.path.join(MEASUREMENTS_PATH, new_name)
-        
-        # Rename the file
-        os.rename(old_path, new_path)
+    encoding = get_file_encoding('test_1.txt')
+    
+    
