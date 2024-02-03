c2f262634e7a803daa4cbc849f2956ba60b981ab
VCOliver <victorcruz.01@gmail.com>
2024-02-03 00:53:08 -0300

    modified stats_table

--- join_files.py
+++ join_files.py
@@ -4,6 +4,7 @@ from chardet import detect
 import pandas as pd
 from tabulate import tabulate
 import sys
+import numpy
 
 MEASUREMENTS_PATH = 'measurements/test_240202'
 MEASUREMENTS_STATS_PATH = 'files_statistics'
@@ -22,27 +23,36 @@ def get_file_encoding(file_name: str) -> str:
     print(f"The file's encoding is [{encoding}] with {result['confidence']*100}% confidence.") 
     return encoding
 
-def main(table_styling='rounded_outline'):
-    encoding = get_file_encoding('test_1.txt')
-    header = ['time', 'x', 'y', 'z']
-    df = pd.read_csv(f'{MEASUREMENTS_PATH}/test_1.txt', sep='\t', decimal=',', encoding=encoding, header=None, names=header)
-    
+def write_stats(df: pd.DataFrame, file_name: str, encoding='utf-8', table_styling='rounded_outline') -> None:
     if not os.path.exists(MEASUREMENTS_STATS_PATH):
         os.makedirs(MEASUREMENTS_STATS_PATH)
     file_stats = df.describe()
-    file_path = f'{MEASUREMENTS_STATS_PATH}/test_1_stats.txt'
+    count = str(int(file_stats.iloc[0, 1]))
+    file_stats.drop('count', inplace=True)
     
+    file_path = f'{MEASUREMENTS_STATS_PATH}/{file_name}'
     table = tabulate(file_stats, headers='keys', tablefmt=table_styling, floatfmt=".7f")
-    
     with open(file_path, 'w', encoding=encoding) as file:
         file.write(table)
+        file.write(f'\n\nNumber of measuments = {count}')
 
+
+def main(table_styling):
+    encoding = get_file_encoding('test_1.txt')
+    header = ['time', 'x', 'y', 'z']
+    df = pd.read_csv(f'{MEASUREMENTS_PATH}/test_1.txt', sep='\t', decimal=',', encoding=encoding, header=None, names=header)
+    if table_styling == None:
+        write_stats(df, 'test_1_stats.txt', encoding=encoding)
+    else:
+        write_stats(df, 'test_1_stats.txt', encoding=encoding, table_styling=table_styling)
+    
+    
 
 if __name__ == '__main__':
     table_styling = None
     if len(sys.argv) > 1:
         table_styling = sys.argv[1]
-        main(table_styling)
-    else:  
-        main()
+        
+    main(table_styling)
+    
     
