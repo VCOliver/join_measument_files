a1d387f0df163b6231fd45a3b1c8d83c86067558
VCOliver <victorcruz.01@gmail.com>
2024-02-04 17:14:05 -0300

    changed logic to dict of pd.DataFrame

--- join_files.py
+++ join_files.py
@@ -9,17 +9,27 @@ from datetime import datetime
 
 MEASUREMENTS_PATH = 'measurements/test_240202'
 MEASUREMENTS_STATS_PATH = 'files_statistics'
+N_OF_FILES = 14
 SAMPLES_PER_LOOP = 16520 # The NI9234 takes 16520 samples than stop to save them in a .txt file
-LOOPS_PER_FILE = 4
 
 printer = pprint.PrettyPrinter()
 pp = printer.pprint
 
+def get_file_creation_time(file_path):
+    try:
+        # Get the file creation time in seconds since the epoch
+        creation_time = os.path.getctime(file_path)
+        
+        # Convert the creation time to a datetime object
+        creation_datetime = datetime.fromtimestamp(creation_time)
+        
+        return creation_datetime
+    
+    except FileNotFoundError:
+        return f"File not found: {file_path}"
+
 def get_file_encoding(file_name: str) -> str:
     file_path = f'{MEASUREMENTS_PATH}/{file_name}'
-    
-    creation_time = os.stat(file_path)
-    print(creation_time)
     
     with open(file_path, 'rb') as file:
         result = detect(file.read())
@@ -29,11 +39,12 @@ def get_file_encoding(file_name: str) -> str:
     print(f"The file's encoding is [{encoding}] with {result['confidence']*100}% confidence.") 
     return encoding
 
-def write_stats(df: pd.DataFrame, file_name: str, encoding='utf-8', table_styling='rounded_outline') -> None:
+def write_stats(df: pd.DataFrame, file_name: str, encoding='utf-8', table_styling='rounded_outline') -> int:
     if not os.path.exists(MEASUREMENTS_STATS_PATH):
         os.makedirs(MEASUREMENTS_STATS_PATH)
+        
     file_stats = df.describe()
-    count = str(int(file_stats.iloc[0, 1]))
+    count = int(file_stats.iloc[0, 1])
     file_stats.drop('count', inplace=True)
     
     file_path = f'{MEASUREMENTS_STATS_PATH}/{file_name}'
@@ -41,8 +52,10 @@ def write_stats(df: pd.DataFrame, file_name: str, encoding='utf-8', table_stylin
     with open(file_path, 'w', encoding=encoding) as file:
         file.write(table)
         file.write(f'\n\nNumber of measuments = {count}')
+    
+    return count
 
-def get_mean_saving_time(df: pd.DataFrame) -> float:
+def get_mean_saving_time(df: pd.DataFrame, LOOPS_PER_FILE: str) -> float:
     saving_time = 0    
     for i in range(1, LOOPS_PER_FILE):
         saving_time+= df.iloc[SAMPLES_PER_LOOP*i, 0] - df.iloc[SAMPLES_PER_LOOP*i-1, 0]
@@ -50,22 +63,38 @@ def get_mean_saving_time(df: pd.DataFrame) -> float:
     
     return saving_time.round(6)
 
+def get_df_list(encoding) -> dict[str, pd.DataFrame]:
+    files = [file for _, _, files in os.walk(MEASUREMENTS_PATH) for file in files]
+    
+    header = ['time', 'x', 'y', 'z']
+    files_dict = {}
+    for file in files:
+        files_dict[file] = pd.read_csv(f'{MEASUREMENTS_PATH}/{file}', sep='\t', decimal=',', encoding=encoding, header=None, names=header)
+
+    return files_dict
+
 def main(table_styling):
     encoding = get_file_encoding('test_1.txt')
-    header = ['time', 'x', 'y', 'z']
-    df = pd.read_csv(f'{MEASUREMENTS_PATH}/test_1.txt', sep='\t', decimal=',', encoding=encoding, header=None, names=header)
     
-    if table_styling in tabulate_formats:
-        write_stats(df, 'test_1_stats.txt', encoding=encoding, table_styling=table_styling)
-    else:
-        write_stats(df, 'test_1_stats.txt', encoding=encoding)
- 
-    saving_time = get_mean_saving_time(df)
-    print(saving_time)
+    files_dict = get_df_list(encoding)
+
+    saving_time = 0
+    for key, value in files_dict.items():
+        df = value
+        stat_file_name = key.replace('.txt', '_stats.txt')
+        if table_styling in tabulate_formats:
+            n_of_readings = write_stats(df, stat_file_name, encoding=encoding, table_styling=table_styling)
+        else:
+            n_of_readings = write_stats(df, stat_file_name, encoding=encoding)
+        
+        loops_per_reading = int(n_of_readings/SAMPLES_PER_LOOP)
+        saving_time += get_mean_saving_time(df, loops_per_reading)
             
-    
+    avrg_saving_time = saving_time/N_OF_FILES
+    print(avrg_saving_time)
 
 if __name__ == '__main__':
+        
     table_styling = None
     if len(sys.argv) > 1:
         table_styling = sys.argv[1]
